a44ee33f74536122152b0b780d80282ab8f168ad
jhmcarvalho <jeferson_funet@hotmail.com>
2021-11-26 19:58:13 -0300

    Dir e Help - Utilitários para ajudar na programação.

---
+++ dir_e_help.py
@@ -0,0 +1,11 @@
+"""
+Utilitários Python para auxiliar na programação.
+
+dir -> Apresenta todos os atributos/propriedades e funções/métodos disponíveis para determinado tipo de dado ou variável.
+-->Como usar: dir(tipo de dado/variável)
+
+help -> Apresenta a documentação/como utilizar os atributos/propriedade e funções/métodos disponíveis para determinado
+tipo de dado ou variável.
+-->Como usar: help(tipo de dado/variavel.propriedade)
+
+"""
