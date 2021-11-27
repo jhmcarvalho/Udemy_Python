10e55c6a23816b1814b4e8ab31fcec1fac8f7adf
jhmcarvalho <jeferson_funet@hotmail.com>
2021-11-27 11:52:51 -0300

    Adicionando exemplos no dir e help

--- dir_e_help.py
+++ dir_e_help.py
@@ -2,10 +2,11 @@
 Utilitários Python para auxiliar na programação.
 
 dir -> Apresenta todos os atributos/propriedades e funções/métodos disponíveis para determinado tipo de dado ou variável.
--->Como usar: dir(tipo de dado/variável)
+-->Como usar: dir(tipo de dado/variável) - Exemplo: dir("Jeferson")
 
 help -> Apresenta a documentação/como utilizar os atributos/propriedade e funções/métodos disponíveis para determinado
 tipo de dado ou variável.
--->Como usar: help(tipo de dado/variavel.propriedade)
+-->Como usar: help(tipo de dado/variavel.propriedade) - Exemplo: help("Jeferson".upper)
+
 
 """
