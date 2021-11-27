5c589907a38b92f3b4828cb3674a3d3ebd7a9d29
jhmcarvalho <jeferson_funet@hotmail.com>
2021-11-27 12:35:11 -0300

    Criando uma lista de atalhos do PyCharm

---
+++ atalhos.py
@@ -0,0 +1,10 @@
+"""
+SHIFT + F10 -- Roda o último arquivo (run normal)
+SHIFT + F9 -- Deguga o último arquivo (run normal + debug)
+ALT + SHIFT + F10 -- Abre a lista para escolher o arquivo que vai executar (run normal)
+ALT + SHIFT + F9 -- Abre a lista para escolher o arquivo que vai executar com debug (run + debug)
+SHIFT + ESC -- Minimiza a barra de projetos do canto
+CTRL + L -- Limpa o terminal
+CTRL + K -- Commita
+
+"""
