650810e7c5af39e3cdf4a4ba48c4d9704b4a9cdb
jhmcarvalho <jeferson_funet@hotmail.com>
2022-03-24 13:32:05 -0300

    Adicionados novos utilitários importantes;

--- Utilitarios_python.py
+++ Utilitarios_python.py
@@ -15,10 +15,22 @@ dir -> Apresenta todos os atributos/propriedades e funções/métodos disponíve
 help -> Apresenta a documentação/como utilizar os atributos/propriedade e funções/métodos disponíveis para determinado
 tipo de dado ou variável.
 -->Como usar: help(tipo de dado/variavel.propriedade) - Exemplo: help("Jeferson".upper)
+"""
 
-print(nome.split()) #Transforma em uma lista de strings;
-print(nome.upper()) #Transforma em maiúsculo;
-print(nome.lower()) #Transforma em minúsculo;
 
+#PRINTS
+nome = 'Jeferson Martins'
 
 """
+print(nome.split()) #Transforma em uma lista de strings;
+print(nome.split()[0]) #Acessa a primeira parte da lista dividida (Jeferson Martins apareceria Jeferson);
+print(nome.split())[1] #Acessa a segunda parte da lista dividida (Jeferson Martins apareceria Martins);
+print(nome.upper()) #Transforma em maiúsculo;
+print(nome.lower()) #Transforma em minúsculo;
+print(nome[0:8]) #Exibe somente parte da string. Nesse caso, da posição 0 até a 8 -- Slice de string;
+print(nome[::-1]) #Inverte tudo. :vai do primeiro elemento, :vai até o ultimo elemento, -1 inverte;
+print(nome.replace('J', 'G')) #Onde possui a letra J vai mudar para G;
+
+"""
+
+
